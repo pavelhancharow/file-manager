35d359a5151ffb797043c363749fb31dad68c047
Pavel Hancharou <pavel.hancharou@itechart-group.com>
2024-02-05 17:36:25 +0100

    feat: implement constants

---
+++ src/constants/OperatingSystemFlags.js
@@ -0,0 +1,9 @@
+const OperatingSystemFlags = {
+  Architecture: '--architecture',
+  CPUS: '--cpus',
+  EOL: '--EOL',
+  HomeDir: '--homedir',
+  Username: '--username',
+};
+
+export default OperatingSystemFlags;
